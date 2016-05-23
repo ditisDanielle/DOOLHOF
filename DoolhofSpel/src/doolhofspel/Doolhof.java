7cd78de5576c7c7045217a19f73f06507cd2ee40
daniellescheide <ditisDanielle@users.noreply.github.com>
2016-05-23 15:38:03 +0200

    framefix

--- DoolhofSpel/src/doolhofspel/Doolhof.java
+++ DoolhofSpel/src/doolhofspel/Doolhof.java
@@ -2,6 +2,7 @@ package doolhofspel;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -10,57 +11,66 @@ import javax.swing.JPanel;
  *
  * @author Agnita & Danielle (Groep 7)
  */
-
 class Doolhof {
-    int buttonSizeHeight;
-    int buttonSizesWidth;
-      
+
+    int buttonSizeHeight = 1; // later FINAL maken
+    int buttonSizesWidth = 3;// later FINAL maken
+    JFrame spelframe;
+    JPanel gamepanel;
+    JPanel buttonpanel;
+    JPanel oostpanel;
+    JPanel dummy1;
+    JPanel dummy2;
+    JPanel dummy3;
+    JPanel dummy4;
+    JButton reset;
+/// BUTTON IN EEN APARTE PANEL > PANEL IN CENTER VAN BORDERLAYOUT BUTTONPANEL 
+    
+    
     public Doolhof() {
         // maak een gameframe
-        JFrame frame = new JFrame();
-        frame.setTitle("Zoek je vriend!");
-        frame.setSize(1000, 1000);
-        // frame.setSize(694, 728); // eerste int is breedte, tweede is lengte, deze maat graag even bewaren!
-        // beter is om de framesize relatief te maken aan de plaatjes (32x32)
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // maak de gamepanel
-        JPanel gamepanel = new JPanel(new BorderLayout());
-        // twee fakepanels, nodig voor de layout (north & south)
-        JPanel north = new JPanel();
-        north.setPreferredSize(new Dimension(5, 50));
-        JPanel south = new JPanel();
-        south.setPreferredSize(new Dimension(5, 250));
-        // een buttonpanel voor de start en de reset buttons
-        JPanel buttonPanel = new JPanel(new BorderLayout());
-        // startbutton toevoegen
-        JButton start = new JButton();
-        // actie toevoegen aan de startbutton!
-        start.addActionListener(null);
-        start.setText("Start");
-        start.setSize(buttonSizeHeight, buttonSizesWidth);
-        buttonPanel.add(start, BorderLayout.NORTH);
-        // resetbutton toevoegen
-        JButton reset = new JButton();
+        spelframe = new JFrame();
+        spelframe.setTitle("Zoek je vriend!");
+        spelframe.setSize(1000, 1000);
+        spelframe.setLocationRelativeTo(null);
+        spelframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // set layouts
+        gamepanel = new JPanel(new GridLayout(1, 2));
+        buttonpanel = new JPanel(new BorderLayout());
+        oostpanel = new JPanel(new BorderLayout());
+
+        //button
+        reset = new JButton();
+        reset.setSize(buttonSizeHeight, buttonSizesWidth);
+        reset.setText("Reset");
+        
         // actie toevoegen aan de resetbutton!
         reset.addActionListener(null);
-        reset.setText("Reset");
-        reset.setSize(buttonSizeHeight, buttonSizesWidth);
-        buttonPanel.add(reset, BorderLayout.SOUTH);
-        // fake panel toevoegen ivm de layout
-        JPanel fakePanel = new JPanel();
-        buttonPanel.add(fakePanel, BorderLayout.CENTER);
-        // nog zo'n fakepanel
-        JPanel west = new JPanel();
-        west.setPreferredSize(new Dimension(50, 50));
+
+        buttonpanel.add(reset, BorderLayout.CENTER);
+
+        // dummy panels toevoegen ivm de layout
+        dummy1 = new JPanel();
+        dummy1.setPreferredSize(new Dimension(50, 300));
+        buttonpanel.add(dummy1, BorderLayout.NORTH);
+        dummy2 = new JPanel();
+        dummy2.setPreferredSize(new Dimension(50, 300));
+        buttonpanel.add(dummy2, BorderLayout.SOUTH);
+        dummy3 = new JPanel();
+        dummy3.setPreferredSize(new Dimension(50, 350));
+        buttonpanel.add(dummy3, BorderLayout.EAST);
+        dummy4 = new JPanel();
+        dummy4.setPreferredSize(new Dimension(50, 350));
+        buttonpanel.add(dummy4, BorderLayout.WEST);
+
         // alle panels toevoegen aan het uiteindelijke gamepanel
-        gamepanel.add(new Bord(), BorderLayout.CENTER);
-        gamepanel.add(north, BorderLayout.NORTH);
-        gamepanel.add(south, BorderLayout.SOUTH);
-        gamepanel.add(buttonPanel, BorderLayout.EAST);
-        gamepanel.add(west, BorderLayout.WEST);
+        gamepanel.add(new Bord());
+        gamepanel.add(oostpanel);
+        oostpanel.add(buttonpanel, BorderLayout.CENTER);
+
         // als laatste voegen we de gamepanel toe aan de gameframe en zorgen we dat het zichtbaar is
-        frame.add(gamepanel);
-        frame.setVisible(true);
+        spelframe.add(gamepanel);
+        spelframe.setVisible(true);
     }
 }
